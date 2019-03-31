998ef88d3aa6ac11f2eb0a58c66a9daf71a9d4ee
Daniel <Daniel@DITERION>
2019-03-31 04:43:03 +0200

    added packages

--- src/app/components/account-profile/account-profile.module.ts
+++ src/app/components/account-profile/account-profile.module.ts
@@ -2,11 +2,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main/main.component';
+import { ChartsModule } from 'ng2-charts';
+import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.component';
 
 const routes: Routes = [{ path: 'profile', component: MainComponent }];
 
 @NgModule({
-  declarations: [MainComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)]
+  declarations: [MainComponent, DoughnutChartComponent],
+  imports: [CommonModule, RouterModule.forChild(routes), ChartsModule]
 })
 export class AccountProfileModule {}
