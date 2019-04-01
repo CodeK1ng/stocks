709bb01f93a3cd646abdb2bba424a54fae0a36f9
Daniel <Daniel@DITERION>
2019-04-01 06:43:47 +0200

    prototype ready

--- src/app/components/account/address-info/address-info.component.ts
+++ src/app/components/account/address-info/address-info.component.ts
@@ -1,31 +1,98 @@
+import {
+   FormBuilder,
+   FormGroup,
+   Validators,
+   FormControl,
+   ValidationErrors
+} from '@angular/forms';
 import { Location } from '@angular/common';
 import { StepsService } from './../accounts-service/steps.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
-  selector: 'app-address-info',
-  templateUrl: './address-info.component.html',
-  styleUrls: ['./address-info.component.scss']
+   selector: 'app-address-info',
+   templateUrl: './address-info.component.html',
+   styleUrls: ['./address-info.component.scss']
 })
 export class AddressInfoComponent implements OnInit {
-  current: number;
-  constructor(
-    private steps: StepsService,
-    private route: Router,
-    private location: Location
-  ) {}
+   current: number;
+   validateForm: FormGroup;
+   constructor(
+      private steps: StepsService,
+      private route: Router,
+      private location: Location,
+      private fb: FormBuilder
+   ) {
+      this.validateForm = this.fb.group({
+         userName: ['', [Validators.required], [this.userNameAsyncValidator]],
+         email: ['', [Validators.email, Validators.required]],
+         password: ['', [Validators.required]],
+         confirm: ['', [this.confirmValidator]],
+         comment: ['', [Validators.required]]
+      });
+   }
 
-  onSubmit() {
-    this.route.navigate(['tax-info']);
-  }
+   onSubmit() {
+      this.route.navigate(['tax-info']);
+   }
 
-  onBackClicked() {
-    this.location.back();
-  }
+   onBackClicked() {
+      this.location.back();
+   }
 
-  ngOnInit() {
-    this.steps.currentStep.subscribe(step => (this.current = step));
-    this.steps.changeStep(2);
-  }
+   ngOnInit() {
+      this.steps.currentStep.subscribe(step => (this.current = step));
+      this.steps.changeStep(2);
+   }
+
+   submitForm = ($event: any, value: any) => {
+      $event.preventDefault();
+      for (const key in this.validateForm.controls) {
+         if (this.validateForm.controls.hasOwnProperty(key)) {
+            this.validateForm.controls[key].markAsDirty();
+            this.validateForm.controls[key].updateValueAndValidity();
+         }
+      }
+      console.log(value);
+   };
+
+   resetForm(e: MouseEvent): void {
+      e.preventDefault();
+      this.validateForm.reset();
+      for (const key in this.validateForm.controls) {
+         if (this.validateForm.controls.hasOwnProperty(key)) {
+            this.validateForm.controls[key].markAsPristine();
+            this.validateForm.controls[key].updateValueAndValidity();
+         }
+      }
+   }
+
+   validateConfirmPassword(): void {
+      setTimeout(() =>
+         this.validateForm.controls.confirm.updateValueAndValidity()
+      );
+   }
+
+   userNameAsyncValidator = (control: FormControl) =>
+      Observable.create((observer: Observer<ValidationErrors | null>) => {
+         setTimeout(() => {
+            if (control.value === 'JasonWood') {
+               observer.next({ error: true, duplicated: true });
+            } else {
+               observer.next(null);
+            }
+            observer.complete();
+         }, 1000);
+      });
+
+   confirmValidator = (control: FormControl): { [s: string]: boolean } => {
+      if (!control.value) {
+         return { required: true };
+      } else if (control.value !== this.validateForm.controls.password.value) {
+         return { confirm: true, error: true };
+      }
+      return {};
+   };
 }
--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,15 +1,62 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-admin',
-  templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+   selector: 'app-admin',
+   templateUrl: './admin.component.html',
+   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent implements OnInit {
+   listOfData = [
+      {
+         key: '1',
+         name: 'John Brown',
+         age: 32,
+         address: 'New York No. 1 Lake Park',
+         sum: 0.0159,
+         total: 0.0159,
+         size: 5680.47,
+         bid: 0.0000028,
+         ask: 0.00000281,
+         size2: 360358.19,
+         total2: 1.0126,
+         sum2: 1.0126,
+         date: '02/18/2018 05:36:12 PM',
+         type: 'Limit Sell'
+      },
+      {
+         key: '2',
+         name: 'John Brown',
+         age: 32,
+         address: 'New York No. 1 Lake Park',
+         sum: 0.0159,
+         total: 0.0159,
+         size: 5680.47,
+         bid: 0.0000028,
+         ask: 0.00000281,
+         size2: 360358.19,
+         total2: 1.0126,
+         sum2: 1.0126,
+         date: '02/18/2018 05:36:12 PM',
+         type: 'Limit Buy'
+      },
+      {
+         key: '3',
+         name: 'John Brown',
+         age: 32,
+         address: 'New York No. 1 Lake Park',
+         sum: 0.0159,
+         total: 0.0159,
+         size: 5680.47,
+         bid: 0.0000028,
+         ask: 0.00000281,
+         size2: 360358.19,
+         total2: 1.0126,
+         sum2: 1.0126,
+         date: '02/18/2018 05:36:12 PM',
+         type: 'Market Buy'
+      }
+   ];
+   constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+   ngOnInit() {}
 }
